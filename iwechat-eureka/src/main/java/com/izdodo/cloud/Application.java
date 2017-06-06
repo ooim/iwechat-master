c30097df7b78b304c6f57a022df93178603e1a01
ooim <770144935@qq.com>
2017-06-06 15:14:04 +0800

    1、注册发现服务器 2、parent pom新增spring boot依赖

---
+++ iwechat-eureka/src/main/java/com/izdodo/cloud/Application.java
@@ -0,0 +1,12 @@
+package com.izdodo.cloud;
+
+/**
+ * Description 注册服务器
+ * Created by zengfeiyue 2017-6-6
+ */
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
